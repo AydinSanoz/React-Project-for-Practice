c9a4a0afef1d7037288b747fc2b28fdc6338fc5c
E2029-Aydin <aydinsanoz2@gmail.com>
2020-10-16 21:32:33 +0300

    BestPrac.js created

---
+++ src/BestPrac.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { SafeAreaView, Text, View } from 'react-native';
+
+const App = () => {
+    return(
+        <SafeAreaView>
+            <View>
+                <Text> Welcome on Board </Text>
+            </View>
+        </SafeAreaView>
+    )
+}
+
+export default App;
